1a1a44262f314314e7afdd653246c18ef868527a
Simao Martins <simaopmartins@gmail.com>
2025-02-26 15:33:09 +0000

    yelp MVP

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  
+
   CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
 
   validates :name, :address, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
+10.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: CATEGORIES.sample
+  )
+end
+
+puts "Finished! Created #{Restaurant.count} restaurants."
